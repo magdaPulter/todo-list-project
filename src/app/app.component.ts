4e7f4eb5256e8db05dbc30bb27ec0ca390a76036
magdaPulter <magda.pulter@gmail.com>
2024-08-19 12:05:47 +0200

    date added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, SimpleChanges, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -20,6 +20,10 @@ export class AppComponent {
     project_id: '',
     id: '',
     description: '',
+    is_completed: false,
+    due: {
+      date: '',
+    },
   };
 
   TOKEN: string = '23c109ece8896fe9b8c55fae14c29cdd327373ab';
@@ -27,26 +31,38 @@ export class AppComponent {
     `Authorization`,
     `Bearer ${this.TOKEN}`
   );
+  url: string = 'https://api.todoist.com/rest/v2';
+
+  minDate() {
+    const date = new Date();
+    const m =
+      date.getMonth() + 1 < 10
+        ? `0${date.getMonth() + 1}`
+        : date.getMonth() + 1;
+    const d = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+    const y = date.getFullYear();
+
+    return `${y}-${m}-${d}`;
+  }
 
   getProjects(): Observable<ProjectModel[]> {
-    return this._httpClient.get<ProjectModel[]>(
-      `https://api.todoist.com/rest/v2/projects`,
-      {
-        headers: this.headers,
-      }
-    );
+    return this._httpClient.get<ProjectModel[]>(`${this.url}/projects`, {
+      headers: this.headers,
+    });
   }
   getTask(): Observable<TaskModel[]> {
-    return this._httpClient.get<TaskModel[]>(
-      `https://api.todoist.com/rest/v2/tasks`,
-      {
-        headers: this.headers,
-      }
-    );
+    return this._httpClient.get<TaskModel[]>(`${this.url}/tasks`, {
+      headers: this.headers,
+    });
   }
   createTask(task: TaskModel) {
-    return this._httpClient.post<TaskModel>(
-      `https://api.todoist.com/rest/v2/tasks`,
+    return this._httpClient.post<TaskModel>(`${this.url}/tasks`, task, {
+      headers: this.headers,
+    });
+  }
+  updateTask(task: TaskModel) {
+    return this._httpClient.patch<TaskModel>(
+      `${this.url}/tasks/${task.id}`,
       task,
       {
         headers: this.headers,
@@ -57,10 +73,19 @@ export class AppComponent {
   tasks$: Observable<TaskModel[]> = this.getTask();
   projects$: Observable<ProjectModel[]> = this.getProjects();
 
-  constructor(private _httpClient: HttpClient) {}
+  constructor(private _httpClient: HttpClient) {
+    this.tasks$.subscribe((val) => console.log(val));
+  }
+
+  onCheck(task: TaskModel) {
+    // this.updateTask(task).subscribe();
+  }
 
   onFormSubmitted(form: NgForm) {
     if (form.valid) {
+      // console.log(this.task);
+      console.log(this.task);
+
       this.createTask(this.task).subscribe();
     }
   }
--- src/app/models/task-model.model.ts
+++ src/app/models/task-model.model.ts
@@ -3,4 +3,8 @@ export interface TaskModel {
   readonly content: string;
   readonly description: string;
   readonly project_id: string;
+  readonly is_completed: boolean;
+  readonly due: {
+    readonly date: string;
+  };
 }
