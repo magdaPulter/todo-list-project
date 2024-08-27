102ba702e5d99cebd7ef76e44d97c9602f58fdcb
magdaPulter <magda.pulter@gmail.com>
2024-08-27 11:59:07 +0200

    dynamic route set

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: TodoListComponent,
+  },
+  {
+    path: 'tasks/:id',
+    component: TaskDetailComponent,
   },
 ];
