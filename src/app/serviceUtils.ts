48c8824afd87edd5be1ff4a793f1e8be7c5e5765
magdaPulter <magda.pulter@gmail.com>
2024-10-30 18:09:10 +0100

    token added

--- src/app/serviceUtils.ts
+++ src/app/serviceUtils.ts
@@ -4,7 +4,8 @@ import { environment } from '../environment/environment';
 export const servicesUtils = {
   headers: new HttpHeaders().set(
     `Authorization`,
-    `Bearer ${environment.TOKEN}`
+    `Bearer 23c109ece8896fe9b8c55fae14c29cdd327373ab`
+    // `Bearer ${environment.TOKEN}`
   ),
   url: 'https://api.todoist.com/rest/v2',
 };
