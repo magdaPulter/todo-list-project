8b603b7144e90e57859673b20c712b7dc1c4b236
magdaPulter <magda.pulter@gmail.com>
2024-11-21 18:11:27 +0100

    credential hidden

--- src/app/serviceUtils.ts
+++ src/app/serviceUtils.ts
@@ -4,8 +4,7 @@ import { environment } from '../environment/environment';
 export const servicesUtils = {
   headers: new HttpHeaders().set(
     `Authorization`,
-    `Bearer 23c109ece8896fe9b8c55fae14c29cdd327373ab`
-    // `Bearer ${environment.TOKEN}`
+    `Bearer ${environment.TOKEN}`
   ),
   url: 'https://api.todoist.com/rest/v2',
 };
